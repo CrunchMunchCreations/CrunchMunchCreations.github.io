df05d5ed71cb615cb2fdea12953feae6647a0f4a
Naz <bluspringindohq@gmail.com>
2023-12-17 21:06:19 +0800

    More mobile adjustments

--- components/FooterComp.vue
+++ components/FooterComp.vue
@@ -1,25 +1,49 @@
 <template>
-    <div class="bg-[#61809f] w-screen bottom-0 h-36 relative">
-        <div class="our-black sm:text-2xl float-left flex-row items-center align-middle">
-            <div class="m-auto absolute w-max h-fit top-0 bottom-0 text-center">
-                <a href="/terms" class="underline mx-8 self-center hover:brightness-200">Terms & Conditions</a> <br>
-                <a href="/privacy" class="underline mx-8 self-center hover:brightness-200">Privacy Policy</a>
+    <div class="bg-[#61809f] w-screen bottom-0 h-60 md:h-36 relative">
+        <div class="desktop md:block hidden">
+            <div class="our-black sm:text-2xl float-left flex-row items-center align-middle">
+                <div class="m-auto absolute w-max h-fit top-0 bottom-0 text-center">
+                    <a href="/terms" class="underline mx-8 self-center hover:brightness-200">Terms & Conditions</a> <br>
+                    <a href="/privacy" class="underline mx-8 self-center hover:brightness-200">Privacy Policy</a>
+                </div>
             </div>
+
+            <div class="our-black text-4xl flex-row float-right items-center align-middle">
+                <div class="items-center align-middle my-auto mx-auto absolute top-0 bottom-0 left-0 right-0 text-center w-max h-fit">
+                    <a href="https://twitter.com/CrunchMunch_" class="mx-2 hover:brightness-200"><i class="fab fa-twitter"></i></a>
+                    <a href="https://discord.gg/rMuHUKzKXS" class="mx-2 hover:brightness-200"><i class="fab fa-discord"></i></a>
+                    <a href="https://github.com/CrunchMunchCreations" class="mx-2 hover:brightness-200"><i class="fab fa-github"></i></a>
+                    <a href="mailto:crunchmunchcreations@gmail.com" class="mx-2 hover:brightness-200"><i class="fas fa-envelope"></i></a>
+                </div>
+            </div>
+
+            <div class="block sm:hidden relative mx-auto">
+                <p class="text-center our-black -top-8 absolute right-0 left-0 w-48 mx-auto my-8 sm:text-xl">Not affiliated with Mojang Studios or Microsoft.</p>
+            </div>
+            <p class="text-center our-black top-0 absolute right-8 w-48 mx-auto my-8 sm:text-xl hidden sm:block">Not affiliated with Mojang Studios or Microsoft.</p>
         </div>
 
-        <div class="our-black text-4xl flex-row float-right items-center align-middle">
-            <div class="items-center align-middle my-auto mx-auto absolute top-0 bottom-0 left-0 right-0 text-center w-max h-fit">
-                <a href="https://twitter.com/CrunchMunch_" class="mx-2 hover:brightness-200"><i class="fab fa-twitter"></i></a>
-                <a href="https://discord.gg/rMuHUKzKXS" class="mx-2 hover:brightness-200"><i class="fab fa-discord"></i></a>
-                <a href="https://github.com/CrunchMunchCreations" class="mx-2 hover:brightness-200"><i class="fab fa-github"></i></a>
-                <a href="mailto:crunchmunchcreations@gmail.com" class="mx-2 hover:brightness-200"><i class="fas fa-envelope"></i></a>
+        <div class="mobile block md:hidden">
+            <div class="our-black text-2xl pt-4 flex-row items-center align-middle">
+                <div class="m-auto w-max h-fit top-0 bottom-0 text-center">
+                    <a href="/terms" class="underline mx-8 self-center hover:brightness-200">Terms & Conditions</a> <br>
+                    <a href="/privacy" class="underline mx-8 self-center hover:brightness-200">Privacy Policy</a>
+                </div>
+            </div>
+
+            <div class="our-black text-4xl flex-row items-center align-middle text-center mx-auto">
+                <div class="items-center align-middle my-4 mx-auto text-center w-max h-fit">
+                    <a href="https://twitter.com/CrunchMunch_" class="mx-2 hover:brightness-200"><i class="fab fa-twitter"></i></a>
+                    <a href="https://discord.gg/rMuHUKzKXS" class="mx-2 hover:brightness-200"><i class="fab fa-discord"></i></a>
+                    <a href="https://github.com/CrunchMunchCreations" class="mx-2 hover:brightness-200"><i class="fab fa-github"></i></a>
+                    <a href="mailto:crunchmunchcreations@gmail.com" class="mx-2 hover:brightness-200"><i class="fas fa-envelope"></i></a>
+                </div>
+            </div>
+
+            <div class="block mx-auto">
+                <p class="text-center our-black w-48 mx-auto text-xl">Not affiliated with Mojang Studios or Microsoft.</p>
             </div>
         </div>
-
-        <div class="block sm:hidden relative mx-auto">
-            <p class="text-center our-black -top-8 absolute right-0 left-0 w-48 mx-auto my-8 sm:text-xl">Not affiliated with Mojang Studios or Microsoft.</p>
-        </div>
-        <p class="text-center our-black top-0 absolute right-8 w-48 mx-auto my-8 sm:text-xl hidden sm:block">Not affiliated with Mojang Studios or Microsoft.</p>
     </div>
 </template>
 
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="bg-[#ef8aa8] w-screen lg:h-28 py-2 top-0 left-0 z-50 font-header text-[#230d05] text-3xl sm:text-3xl md:text-4xl text-center">
-        <a href="/" class="mx-8 hover:brightness-200">HOME</a>
-        <a href="/projects" class="mx-8 hover:brightness-200">PROJECTS</a>
-        <img src="/chompers.png" class="inline h-24 mx-8">
-        <a href="/about" class="mx-8 hover:brightness-200">ABOUT</a>
-        <a href="/contact" class="mx-8 hover:brightness-200">CONTACT</a>
+    <div class="bg-[#ef8aa8] w-screen lg:h-28 py-2 top-0 left-0 z-50 font-header text-[#230d05] text-2xl sm:text-3xl md:text-4xl text-center">
+        <a href="/" :class="classes">HOME</a>
+        <a href="/projects" :class="classes">PROJECTS</a>
+        <a href="/" :class="classes"><img src="/chompers.png" class="h-12 md:h-24 inline"></a>
+        <a href="/about" :class="classes">ABOUT</a>
+        <a href="/contact" :class="classes">CONTACT</a>
     </div>
 </template>
 
 <script lang="ts" setup>
-
+const classes = 'mx-1 md:mx-2 lg:mx-8 hover:brightness-200';
 </script>
 
 <style>
