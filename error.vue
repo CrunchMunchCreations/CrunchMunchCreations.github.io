f9d4bedf173433e4912ff0ab4e6cc2f0b26ffc7b
Naz <bluspringindohq@gmail.com>
2023-12-19 06:06:21 +0800

    Add meta tags

--- error.vue
+++ error.vue
@@ -27,7 +27,44 @@
     const route = useRoute();
 
     useHead({
-        meta: [{ property: 'og:title', content: `${route.meta.title} - CrunchMunch Creations` }],
+        meta: [
+        { 
+                property: 'og:title', 
+                content: `${route.meta.title} - CrunchMunch Creations` 
+            },
+            {
+                property: 'og:type',
+                content: 'website'
+            },
+            {
+                property: 'og:image',
+                content: 'https://crunchmunch.xyz/chompers.png'
+            },
+            {
+                property: 'og:url',
+                content: 'https://crunchmunch.xyz'
+            },
+            {
+                property: 'og:description',
+                content: 'CrunchMunch Creations is a team of 3 Minecraft creators who enjoy creating whatever we like, collecting all of our projects under one name.'
+            },
+            {
+                property: 'twitter:domain',
+                content: 'https://crunchmunch.xyz'
+            },
+            {
+                name: 'twitter:title',
+                content: `${route.meta.title} - CrunchMunch Creations`
+            },
+            {
+                name: 'twitter:description',
+                content: 'CrunchMunch Creations is a team of 3 Minecraft creators who enjoy creating whatever we like, collecting all of our projects under one name.'
+            },
+            {
+                name: 'twitter:image',
+                content: 'https://crunchmunch.xyz/chompers.png'
+            }
+        ],
         titleTemplate: '%s - CrunchMunch Creations'
     });
 </script>
