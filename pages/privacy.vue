416c393dea28e2a982691f127257f5738ef613d4
Naz <bluspringindohq@gmail.com>
2023-12-18 17:12:34 +0800

    Add privacy and terms

---
+++ pages/privacy.vue
@@ -0,0 +1,146 @@
+<template>
+    <Title>Privacy Policy</Title>
+
+    <div class="w-screen overflow-hidden text-center">
+        <div class="w-2/3 md:w-1/2 mx-auto">
+            <h3 class="font-header our-black text-5xl mt-12">Privacy Policy</h3>
+            <h5 class="our-black text-2xl my-8">Last updated 18th December 2023</h5>
+
+            <div class="text-xl">
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Definitions</h3>
+                    
+                    <ul class="text-left pl-5 list-disc" style="line-height: 2rem;">
+                        <li><strong>"CrunchMunch Creations"</strong> refers to the staff team of CrunchMunch Creations.</li>
+                        <li><strong>"CrunchMunch Creations organizers"</strong> refers to the organizers of CrunchMunch Creations, who are Zuite, BluSpring, and FlopsyMimi.</li>
+                        <li><strong>"CrunchMunch Creations website"</strong> refers to this website (www.crunchmunch.xyz) as well as subdomains of crunchmunch.xyz (such as www.subdomain.crunchmunch.xyz)</li>
+                    </ul>
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">This Website</h3>
+
+                    CrunchMunch Creations does not see or collect any user information directly from the CrunchMunch Creations website whatsoever.
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Events</h3>
+
+                    CrunchMunch Creations may collect the following information from any players in our events:
+
+                    <ul class="text-left pl-5 list-disc" style="line-height: 2rem;">
+                        <li>name and/or nickname</li>
+                        <li>pronouns</li>
+                        <li>age bracket (13-15, 16-17, 18+)</li>
+                        <li>email address</li>
+                        <li>Discord ID</li>
+                        <li>Minecraft IGN and UUID</li>
+                        <li>profile picture</li>
+                        <li>social links, such as YouTube, Twitch and Twitter</li>
+                        <li>IP address (if you join one of our Minecraft servers)</li>
+                        <li>publicly available information, such as your public profile information or stream analytics</li>
+                    </ul>
+
+                    We collect this in order to:
+
+                    <ul class="text-left pl-5 list-disc" style="line-height: 2rem;">
+                        <li>allow players to join and play on our Minecraft servers</li>
+                        <li>allow other event functionality, such as team communication channels</li>
+                        <li>send you information about the events you are participating in</li>
+                        <li>promote you and your social media links on our website and socials</li>
+                        <li>collect feedback</li>
+                        <li>provide a safe and comfortable environment for everyone involved in CrunchMunch Creations and CrunchMunch Creations-partnered events</li>
+                    </ul>
+
+                    <br>
+                    We only use your information for the purposes stated above. 
+                    Sensitive data such as IP address is only visible to the CrunchMunch Creations organizers,
+                    with the exception of third parties as described below.
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Feedback & Testimonials</h3>
+
+                    CrunchMunch Creations may collect and store feedback about our events in order to track performance, improve future projects, and use as testimonials for marketing purposes.
+                    We will never publish your feedback publicly, or use it as a testimonial of CrunchMunch Creations, without your consent.
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Customer Service Requests</h3>
+
+                    If you contact us to request assistance, we may collect your contact information as provided directly by you, 
+                    for example your name and/or nickname, email address, social handles or Discord ID, 
+                    as well as any additional information you may choose to share within your message.
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Third Parties</h3>
+
+                    This privacy policy applies solely to information collected by CrunchMunch Creations.
+                    <br><br>
+                    The CrunchMunch Creations website contain links to other website. We highly encourage you to read the privacy statements
+                    of each website that collects personally identifiable information.
+                    <br><br>
+                    CrunchMunch Creations uses the following third-party tools, which may collect additional information in accordance with their overflow-hidden
+                    privacy policies.
+                    <br><br>
+                    Information may be shared with additional third-parties where we are required to do so by law.
+
+                    <table class="w-full my-8" style="line-height: 2.5rem;">
+                        <tr class="font-header text-3xl">
+                            <th>Purpose</th>
+                            <th>Website Links</th>
+                        </tr>
+
+                        <tr>
+                            <td>Website, Repository & Domain Hosting</td>
+                            <td>
+                                <a href="https://docs.github.com/en/site-policy/privacy-policies/github-privacy-statement" class="underline hover:brightness-200">GitHub</a>,
+                                <a href="https://www.cloudflare.com/privacypolicy/" class="underline hover:brightness-200">Cloudflare</a>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Admin tools, such as documents and forms</td>
+                            <td>
+                                <a href="https://policies.google.com/privacy" class="underline hover:brightness-200">Google</a>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Mod & Map Distribution</td>
+                            <td>
+                                <a href="https://modrinth.com/legal/privacy" class="underline hover:brightness-200">Modrinth</a>,
+                                <a href="https://www.overwolf.com/legal/privacy/" class="underline hover:brightness-200">CurseForge</a>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Community & Marketing</td>
+                            <td>
+                                <a href="https://discord.com/privacy" class="underline hover:brightness-200">Discord</a>,
+                                <a href="https://twitter.com/en/privacy" class="underline hover:brightness-200">Twitter</a>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Minecraft Server Hosting</td>
+                            <td>
+                                <a href="https://bloom.host/privacy-policy/" class="underline hover:brightness-200">Bloom Host</a>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Donations & Fundraising</td>
+                            <td>
+                                <a href="https://more.ko-fi.com/privacy" class="underline hover:brightness-200">Ko-Fi</a>,
+                                <a href="https://tiltify.com/pages/privacy-policy" class="underline hover:brightness-200">Tiltify</a>,
+                                <a href="https://www.paypal.com/webapps/mpp/ua/privacy-full" class="underline hover:brightness-200">PayPal</a>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/terms.vue
@@ -0,0 +1,87 @@
+<template>
+    <Title>Terms & Conditions</Title>
+
+    <div class="w-screen overflow-hidden text-center">
+        <div class="w-2/3 md:w-1/2 mx-auto">
+            <h3 class="font-header our-black text-5xl mt-12">Terms & Conditions</h3>
+            <h5 class="our-black text-2xl my-8">Last updated 18th December 2023</h5>
+
+            <div class="text-xl">
+                <div class="my-8">
+                    These terms all apply to anything hosted by CrunchMunch Creations, whether events, Minecraft maps, mods, etc.
+                    <br><br>
+
+                    If you have any questions relating to these terms, or any other terms in whatever we host, you can view our <a class="underline hover:brightness-200" href="/contact">Contact Us</a> page.
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Entry Requirements</h3>
+
+                    You must be aged 16+ to apply for or take part in any CrunchMunch Creations events. To be a member of our Discord or community platforms,
+                    you must be aged 13+.
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Code of Conduct</h3>
+
+                    By applying for or taking part in an event, or being a part of our community platforms such as Discord, you agree to behave
+                    in a reasonable and sportsmanlike manner. This includes the following rules:
+
+                    <br><br>
+                    <ul class="text-left pl-5 list-disc" style="line-height: 2rem;">
+                        <li>Be respectful of others, this includes any staff, players, and community members.</li>
+                        <li>Do not be excessively toxic, rude, attention-seeking, or generally being a bad sport.</li>
+                        <li>Do not use any slurs (including what you can reclaim), or utilize any language or imagery that is discriminatory, suggestive, or sexual.</li>
+                        <li>Do not discuss or display any sensitive, political, or potentially triggering topics.</li>
+                        <li>
+                            Do not use anything that will give an unfair gameplay advantage, or otherwise abusing games - this includes (but is not limited to) exploits,
+                            hacks, "cheat" resource packs, auto-clickers (unless you are using it to spam a bell, those are great), cross-teaming, or deliberately trying
+                            to break the games, servers, or maps.
+                        </li>
+                    </ul>
+
+                    <br>
+
+                    Any further rules relating to a specific event or Discord server can be found on the relevant server / website.
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Marketing Materials</h3>
+
+                    By accepting a position in a CrunchMunch Creations event, you agree to us using your creator name / nickname, profile picture, links,
+                    Minecraft skin / likeness, and any other public-facing information, for the purposes of marketing this event. These may also be used
+                    for promoting CrunchMunch Creations or any of its future events <span class="font-bold">in the context of your participation in this event</span>.
+                    <br><br>
+                    Any events, mods, maps, etc. that are created by CrunchMunch Creations may be photographed, recorded, or livestreamed. This includes both on CrunchMunch
+                    Creations related channels, as well as any personal channels belonging to the participants, that we do not have control over. By accepting a position in
+                    any events, you agree to featuring in such photographs and recordings.
+                    <br><br>
+                    By accepting a position in the event, you agree to us using clips or screenshots of your POV taken during the event, for the purposes of marketing this event.
+                    This may also include future events or CrunchMunch Creations as a whole <span class="font-bold">in the context of your participation in this event</span>,
+                    for example in any trailers. This may be displayed on the CrunchMunch Creations website, on our social medias, in our communications, or in-game. We will not
+                    upload your Twitch VODs, footage edited by you, or any video content that primarily or exclusively focuses on you, to any CrunchMunch Creations-related channels
+                    or platforms, without your informed consent.
+                    <br><br>
+                    We will not use your information, footage or likeness on commercial materials, such as on merchandise or on donation pages, or to imply an endorsement of
+                    CrunchMunch Creations, without your informed consent.
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Violation of these Terms</h3>
+
+                    CrunchMunch Creations reserves the right to block entry to or remove players from our events or community platforms, at any time, without giving any reason or notice.
+                    This may include if we believe you cause risk of harm to an event, server, staff member, player, or community member, based on your actions taken before, during, or
+                    after an event.
+                    <br><br>
+                    Depending on the severity and evidence of the violation, this may also include a temporary or permanent ban from future CrunchMunch Creations events or platforms.
+                </div>
+
+                <div class="my-8">
+                    <h3 class="font-header text-3xl">Complaints & Ban Appeals</h3>
+
+                    If you feel like you have been treated unfairly by any CrunchMunch Creations staff, you may <a href="/contact" class="underline hover:brightness-200">contact us</a>.
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
