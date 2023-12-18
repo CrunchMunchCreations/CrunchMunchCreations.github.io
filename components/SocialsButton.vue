41864e8efaf8099d3619fe40edd342c0214489bf
Naz <bluspringindohq@gmail.com>
2023-12-19 04:25:22 +0800

    new tab pls

--- components/SocialsButton.vue
+++ components/SocialsButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :href="href" class="socials-button w-72 hover:w-80 h-10 transition-all relative block text-[#fffcf3] py-2 px-5 rounded-full hover:brightness-125 m-2"  :style="`background: linear-gradient(to top right, #7777AF, ${color})`">
+    <a :href="href" target=”_blank” class="socials-button w-72 hover:w-80 h-10 transition-all relative block text-[#fffcf3] py-2 px-5 rounded-full hover:brightness-125 m-2"  :style="`background: linear-gradient(to top right, #7777AF, ${color})`">
         <span class="absolute left-0 ml-4">{{ name }}</span> 
         <span class="absolute right-0 mr-4"><i :class="icon"></i></span>
     </a>
