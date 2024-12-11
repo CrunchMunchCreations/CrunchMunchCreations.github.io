e4a296c134339dbf8e92881793dac88659ec2d3a
Naz <bluspringindohq@gmail.com>
2024-12-11 19:04:29 +0800

    Add Tiltify redirect

---
+++ pages/christmas.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="text-center h-[70vh] text-4xl my-14">
+        Redirecting to Tiltify... If you have not been redirected, click <a class="underline" href="https://tilt.fyi/dnWwXvSGAx">here</a>.
+    </div>
+</template>
+
+<script lang="ts">
+    
+
+    export default {
+        setup() {
+            useHead({
+                meta: [
+                    {
+                        "http-equiv": 'refresh',
+                        content: '0; url=\'https://tilt.fyi/dnWwXvSGAx\''
+                    }
+                ]
+            });
+        }
+    }
+</script>
+
+<style>
+
+</style>
