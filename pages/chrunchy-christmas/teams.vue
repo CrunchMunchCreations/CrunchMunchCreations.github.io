bb504fcb719ba932e4514dae11cc3bc33b6c2aec
Naz <bluspringindohq@gmail.com>
2025-10-18 19:39:31 +0800

    there's your nitpick

--- pages/chrunchy-christmas/teams.vue
+++ pages/chrunchy-christmas/teams.vue
@@ -4,7 +4,7 @@
     <div class="w-screen flex flex-col christmas-bg p-2 lg:p-12 min-h-screen">
         <div class="text-center w-1/2 mx-auto my-12">
             <h1 class="font-header uppercase text-4xl md:text-7xl">Meet the Teams!</h1>
-            <p class="text-sm md:text-2xl font-semibold">The teams for the 2025 Chrunchy Christmas event has yet to be announced.</p>
+            <p class="text-sm md:text-2xl font-semibold">The teams for the 2025 Chrunchy Christmas event have yet to be announced.</p>
         </div>
         <div class="rounded-lg bg-[#04231d] p-2 lg:p-12 w-full sm:w-4/5 md:w-2/3 xl:w-3/4 3xl:w-1/2 mx-auto flex flex-col">
             <TeamInfo name="Team Crimson" id="crimson">
